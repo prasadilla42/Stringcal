7b0ac694ef0da016f61d9f12f73ac87ac80bdd1f
prasadilla42 <prasadilla42@gmail.com>
2023-03-16 19:11:48 +0000

    logging updated

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, jsonify
 from flask.helpers import flash, redirect, url_for
 from src.exception import NegativeErrorException
 from src.utils import Handlers
+from src.logger import logging
 
 app = Flask(__name__)
 
@@ -15,9 +16,11 @@ def home_page():
 def math_operation():
     if (request.method=='POST'):        
         text = request.form['inputStringBox']
+        logging.info("text is entered")
         handler=Handlers()
         try: 
             numbers=handler.handle_input_string(text)
+            logging.info("numbers are identified")
         except NegativeErrorException as e:
             flash('Error {} occured,Do not enter negative numbers'.format(e.message))
             return redirect(url_for('home_page'))            
@@ -25,6 +28,7 @@ def math_operation():
             flash('Enter minimum 2 numbers in the text')
             return redirect(url_for('home_page'))
         result = sum(numbers)
+        logging.info("Result obtained")
         return render_template('results.html',result=result)
     
 
